3aff85981a08598384ef9a690ea136293aff7e23
Starpuff <ioana.hanus1@gmail.com>
2023-05-27 14:14:59 +0300

    separated search-events page from search-locations

--- client/script.js
+++ client/script.js
@@ -4,7 +4,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   var formData = new FormData();
   formData.append("username", document.getElementById("username").value);
   formData.append("password", document.getElementById("password").value);
-
+  window.location.href = "../search-events/search-events.html";
   fetch('http://localhost:5000/api/login', {
     method: 'POST',
     body: formData
@@ -13,6 +13,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     return response.json();
   })
   .then(function(data) {
+   
     // Process the response data
     if (data === -1) {
       alert("Invalid username or password");
@@ -20,7 +21,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
       // Store the user id in session storage
       sessionStorage.setItem("userId", data);
       // Redirect to search page or perform any other action based on the user id
-      window.location.href = "../search/search.html";
+      //window.location.href = "../search/search.html";
     }
   })
   .catch(function(error) {
