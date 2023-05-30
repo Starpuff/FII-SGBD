bf96a7a455718ed4c4d452d827a1124432dfc212
Ioana Hanus <ioana.hanus@gmail.com>
2023-05-30 14:03:25 +0300

    da un push te rog

--- client/admin-search-locations/adm-locations.js
+++ client/admin-search-locations/adm-locations.js
@@ -70,6 +70,60 @@ const searchLocDescButton = document.querySelector('.loc-desc-button');
 // Add event listener to the button
 searchLocDescButton.addEventListener('click', getByDescription);
 
+const searchLocAddrButton = document.querySelector('.loc-addr-button');
+
+searchLocAddrButton.addEventListener('click', getByAddress);
+
+function getByAddress() {
+  console.log('buttonPressed rttyr');
+  
+  const address = document.getElementById('location-name').value;
+  console.log(address);
+  const locationGrid = document.getElementById('location-grid');
+  locationGrid.innerHTML = '';
+
+  var url = 'http://localhost:5000/api/locations/address/' + address;
+  fetch(url, {
+    method: 'GET',
+  })
+    .then(response => {console.log(response); return response.json()})
+    .then(data => {
+      console.log(data);
+      
+      data.forEach(location => {
+        const locationCard = document.createElement('div');
+        locationCard.className = 'location';
+
+        const locationName = document.createElement('h3');
+        locationName.textContent = location.name;
+        locationCard.appendChild(locationName);
+
+        const locationAddress = document.createElement('p');
+        locationAddress.textContent = `Location: ${location.address}`;
+        locationCard.appendChild(locationAddress);
+
+        const locationCapacity = document.createElement('p');
+        locationCapacity.textContent = `Capacity: ${location.capacity}`;
+        locationCard.appendChild(locationCapacity);
+
+        const locationDescription = document.createElement('p');
+        locationDescription.textContent = `Description: ${location.description}`;
+        locationCard.appendChild(locationDescription);
+
+        const deleteButton = document.createElement('div');
+        deleteButton.className = 'delete-location-button'; 
+        const delButton = createButton('delete-location','trash.png');
+        deleteButton.appendChild(delButton);
+        locationCard.appendChild(deleteButton);
+
+        locationGrid.appendChild(locationCard);
+    });
+    })
+    .catch(error => {
+      alert('Error getting locations!');
+    });
+}
+
 function getByDescription() {
   console.log('buttonPressed');
 
